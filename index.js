b31fd30fe99dd8cf81702307aa165f4abc4b2c97
BSRCIPHER <121090093+BSRCIPHER@users.noreply.github.com>
2025-12-09 15:53:36 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.use(express.json());
+app.use(cors());
+
+app.post('/reverse-url', async (req, res) => {
+    const { url } = req.body;
+     console.log(`Reversing URL: ${url}`);
+    if (!url) {
+        return res.status(400).json({ error: 'URL is required' });
+    }
+
+    try {
+       
+        const response = await axios.get(url);
+        console.log(`Received response with status: ${response.data}`);
+        res.json({ data: response.data });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to reverse URL' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
